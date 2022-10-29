1c9acba0a2cd51193a37f07b5bea8d9e5465a2e1
ngocduc <phamngocduc2000@gmail.com>
2022-10-29 13:21:40 +0700

    update

--- QuanLyQuanCafe/fTableManager.cs
+++ QuanLyQuanCafe/fTableManager.cs
@@ -19,7 +19,6 @@ namespace QuanLyQuanCafe
 
         private void InforToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Console.WriteLine(123);
             fAccountProfile f = new fAccountProfile();
             f.ShowDialog();
         }
